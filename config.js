e29195b02a76f9ade97a9178a318585185156693
kikimyomyo <request1004@gmail.com>
2026-05-08 22:21:54 +0900

    deploy: english-12months

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "english-12months",
+  title: "영어 12달 스펠링",
+  sheet_tab: "english-12months",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
